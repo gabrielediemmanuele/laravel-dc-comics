94a6aad4a2d100a66c23d40e9fba29c27328a77e
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-18 18:03:29 +0200

    feat: change content in navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,7 @@
 @section('main-content')
   <section class="container mt-5">
     <h1>{{ $title }}</h1>
+    <p>Clicca su Comics per visualizzare la tabella!</p> <br>
+    <p>Per vedere i dettagli di un fumetto, clicca sull'icona view!</p>
   </section>
 @endsection
