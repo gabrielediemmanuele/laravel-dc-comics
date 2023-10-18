7a5aa08ce93279239a239d659d448b8e3c015957
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-18 18:00:36 +0200

    feat: add a show system cards

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('main-content')
+<div class="container mt-4 mb-3">
+    <div class="card" style="width: 20rem;">
+        <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
+        <div class="card-header">
+        <div><strong>ID: {{ $comic->id}} </strong></div>
+        <div><strong>Title: {{ $comic->title}}</strong></div>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item"><strong>Price: </strong>{{$comic->price}}</li>
+            <li class="list-group-item"><strong>Series: </strong>{{$comic->series}}</li>
+            <li class="list-group-item"><strong>Sale Date: </strong>{{$comic->sale_date}}</li>
+            <li class="list-group-item"><strong>Type: </strong>{{$comic->type}}</li>
+            <li class="list-group-item"><strong>Create at: </strong>{{$comic->created_at}}</li>
+            <li class="list-group-item"><strong>Update at: </strong>{{$comic->updated_at}}</li>
+            <li class="list-group-item"><strong>Description: </strong>{{$comic->description}}</li>
+        </ul>
+    </div>
+</div>
+@endsection
