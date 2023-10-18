0f1b3bf328e44e8ce09c1fb5cf72c4e717a55aaf
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-18 17:28:34 +0200

    feat: add comics table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,6 +2,35 @@
 
 @section('main-content')
   <section class="container mt-5">
-   
+   <h1> Comics List </h1>
+   <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Title</th>
+        <th scope="col">Price</th>
+        <th scope="col">Series</th>
+        <th scope="col">Sale Date</th>
+        <th scope="col">Type</th>
+        <th scope="col">Creation Date</th>
+        <th scope="col">Last Update</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($comics as $comic)
+      <tr>
+        <td scope="col">{{$comic->id}}</td>
+        <td scope="col">{{$comic->title}}</td>
+        <td scope="col">{{$comic->price}}</td>
+        <td scope="col">{{$comic->series}}</td>
+        <td scope="col">{{$comic->sale_date}}</td>
+        <td scope="col">{{$comic->type}}</td>
+        <td scope="col">{{$comic->created_at}}</td>
+        <td scope="col">{{$comic->updated_at}}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+
   </section>
 @endsection
