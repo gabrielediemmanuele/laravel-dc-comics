33471f73ba93cea416f578f65be8c4b8189db7f6
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-19 16:14:20 +0200

    feat: change nav logo size

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-sm navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand text-primary" href="{{ route('home') }}">DC COMICS</a>
+    <a class="navbar-brand text-primary fw-bold fs-3" href="{{ route('home') }}">DC COMICS</a>
     <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId"
       aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -18,8 +18,8 @@
           <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown"
             aria-haspopup="true" aria-expanded="false">Dropdown</a>
           <div class="dropdown-menu" aria-labelledby="dropdownId">
-            <a class="dropdown-item" href="#">Scarica Illegalmente</a>
-            <a class="dropdown-item" href="#">Solo per adulti</a>
+            <a class="dropdown-item text-warning" href="#">Scarica Illegalmente</a>
+            <a class="dropdown-item text-danger" href="#">Solo per adulti</a>
           </div>
         </li>
       </ul>
