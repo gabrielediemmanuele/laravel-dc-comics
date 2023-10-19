23ece568e9eb1fcc9add57e10a9d45b04319546c
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-19 15:47:29 +0200

    feat: add delete and modal

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -48,9 +48,33 @@ crossorigin="anonymous" referrerpolicy="no-referrer" />
             <form action="{{ route('comics.destroy', $comic) }}" method="POST" class="mx-2">
               @csrf
               @method('DELETE')
-              <a href="{{ route('comics.show', $comic) }}">
+              <a href="#" class="mx-1" data-bs-toggle="modal" data-bs-target="#delete-modal-{{$comic->id}}">
                 <i class="fa-solid fa-trash fa-xl text-danger"></i>
               </a>
+
+              {{--* MODAL --}}
+              <div class="modal fade" id="delete-modal-{{$comic->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="delete-modal-{{$comic->id}}">Modal title</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      ...
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                      <form action="{{ route('comics.destroy', $comic) }}" method="POST" class="mx-2">
+                        @csrf
+                        @method('DELETE')
+                      <button type="button" class="btn btn-danger">Save changes</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </form>
         </td>
       </tr>
