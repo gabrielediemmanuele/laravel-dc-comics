be919d6713f325ab81cdef9884c7e603260b3a0a
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-20 18:50:20 +0200

    fix old

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -24,7 +24,7 @@
             {{-- for visualize correct the form use @csrf protect from fake dates --}}
             <div class="col-3">
                 <label for="title" class="form-label">Title</label>
-                <input type="text" id="title" name="title" class="form-control @error('title') is-invalid @enderror" value="{{ olg('') }}">
+                <input type="text" id="title" name="title" class="form-control @error('title') is-invalid @enderror" value="{{ old('') }}">
                 @error('title')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -34,7 +34,7 @@
 
             <div class="col-3">
                 <label for="price" class="form-label">Price</label>
-                <input type="text" id="price" name="price" class="form-control @error('price') is-invalid @enderror" value="{{ olg('') }}">
+                <input type="text" id="price" name="price" class="form-control @error('price') is-invalid @enderror" value="{{ old('') }}">
                 @error('price')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -44,7 +44,7 @@
 
             <div class="col-3">
                 <label for="series" class="form-label">Series</label>
-                <input type="text" id="series" name="series" class="form-control @error('series') is-invalid @enderror" value="{{ olg('') }}">
+                <input type="text" id="series" name="series" class="form-control @error('series') is-invalid @enderror" value="{{ old('') }}">
                 @error('series')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -54,7 +54,7 @@
 
             <div class="col-3">
                 <label for="sale_date" class="form-label">Sale Date</label>
-                <input type="text" id="sale_date" name="sale_date" class="form-control @error('sale_date') is-invalid @enderror" value="{{ olg('') }}">
+                <input type="text" id="sale_date" name="sale_date" class="form-control @error('sale_date') is-invalid @enderror" value="{{ old('') }}">
                 @error('sale_date')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -64,7 +64,7 @@
 
             <div class="col-3">
                 <label for="type" class="form-label">Type</label>
-                <input type="text" id="type" name="type" class="form-control @error('type') is-invalid @enderror" value="{{ olg('') }}">
+                <input type="text" id="type" name="type" class="form-control @error('type') is-invalid @enderror" value="{{ old('') }}">
                 @error('type')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -74,7 +74,7 @@
 
             <div class="col-12" class="form-label">
                 <label for="description">Description</label>
-                <textarea type="text" id="description" name="description" class="form-control @error('description') is-invalid @enderror" value="{{ olg('') }}" ></textarea>
+                <textarea type="text" id="description" name="description" class="form-control @error('description') is-invalid @enderror" value="{{ old('') }}" ></textarea>
                 @error('description')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -84,7 +84,7 @@
 
             <div class="col-12">
                 <label for="thumb" class="form-label">Thumb</label>
-                <input type="text" id="thumb" name="thumb" class="form-control @error('thumb') is-invalid @enderror" value="{{ olg('') }}">
+                <input type="text" id="thumb" name="thumb" class="form-control @error('thumb') is-invalid @enderror" value="{{ old('') }}">
                 @error('thumb')
                 <div class="invalid-feedback">
                     {{ $message }}
